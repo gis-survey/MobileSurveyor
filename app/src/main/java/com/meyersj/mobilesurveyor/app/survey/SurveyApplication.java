cb34ae8e8b8a887b33d5bd16e80707759b63419a
Jeffrey Meyers <meyers2@pdx.edu>
2015-07-14 16:28:59 -0700

    remove using ARCA for error reporting

--- app/src/main/java/com/meyersj/mobilesurveyor/app/survey/SurveyApplication.java
+++ app/src/main/java/com/meyersj/mobilesurveyor/app/survey/SurveyApplication.java
@@ -15,7 +15,7 @@ public class SurveyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        ACRA.init(this);
+        //ACRA.init(this);
     }
 
 }
